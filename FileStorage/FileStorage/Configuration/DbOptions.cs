8dacdaf13f22081fd7c1036e86eec26222f25ade
Amadeusz Wach <amadeusz.wach@scz.pl>
2019-08-11 21:39:46 +0200

    Some fixes (remove reference to nugget)

---
+++ FileStorage/FileStorage/Configuration/DbOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileStorage.Configuration
+{
+    public class DbOptions
+    {
+        public string Connection { get; set; }
+        public DbType Type { get; set; }
+    }
+
+    public enum DbType
+    {
+        Mysql,
+        Postgresql,
+        Mssql
+    }
+}
